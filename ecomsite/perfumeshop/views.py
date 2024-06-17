56597bd72065c8e931085f7a07ec0df1daab45d5
michalcynarski <cynarski60@gmail.com>
2024-06-17 10:43:32 +0200

    search by price

--- ecomsite/perfumeshop/views.py
+++ ecomsite/perfumeshop/views.py
@@ -1,16 +1,33 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Perfume
-# Create your views here.
 
 def index(request):
-    perfumes = Perfume.objects.all()
+    perfumes = Perfume.objects.all().order_by()
 
     # search code
     item_name = request.GET.get('item_name', '').strip()
 
-    if item_name != '' and item_name is not None:
+    if item_name:
         perfumes = perfumes.filter(name__icontains=item_name) | perfumes.filter(brand__icontains=item_name)
+
+    # price selector code
+    min_price = request.GET.get('min_price', '').strip()
+    max_price = request.GET.get('max_price', '').strip()
+
+    if min_price:
+        try:
+            min_price = float(min_price)
+            perfumes = perfumes.filter(price__gte=min_price)
+        except ValueError:
+            pass
+
+    if max_price:
+        try:
+            max_price = float(max_price)
+            perfumes = perfumes.filter(price__lte=max_price)
+        except ValueError:
+            pass
 
     # pagination code
     paginator = Paginator(perfumes, 24)
@@ -20,7 +37,7 @@ def index(request):
         perfumes = paginator.page(page)
     except PageNotAnInteger:
         perfumes = paginator.page(1)
-
+    except EmptyPage:
+        perfumes = paginator.page(paginator.num_pages)
 
     return render(request, 'perfumeshop/index.html', {'perfumes': perfumes})
-
