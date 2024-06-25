e568e0654916b46a8fe86f10fb377386e730e6c8
cynarski <cynarski60@gmail.com>
2024-06-25 23:57:10 +0200

    add popover

--- ecomsite/perfumeshop/static/perfumeshop/custom.js
+++ ecomsite/perfumeshop/static/perfumeshop/custom.js
@@ -20,7 +20,20 @@ else{
 }
 
 $(document).on('click', '.atc', function(){
-    console.log("Button clicked");
     var item_id = this.id.toString();
     console.log(item_id);
+
+    if(cart[item_id]!=undefined){
+        cart[item_id] = cart[item_id] + 1;
+    }
+    else{
+        cart[item_id] = 1;
+    }
+    console.log(cart);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.getElementById('cart').innerHTML = "Cart("+ Object.keys(cart).length + ")";
 });
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
