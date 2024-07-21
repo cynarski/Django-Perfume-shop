198b093793292ddf473e7352c47c2d03b7e08916
michalcynarski <cynarski60@gmail.com>
2024-07-21 16:42:46 +0200

    Add tables to database

--- ecomsite/perfumeshop/admin.py
+++ ecomsite/perfumeshop/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Perfume
+from .models import *
 # Register your models here.
 
 admin.site.register(Perfume)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
