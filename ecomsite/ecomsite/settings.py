02139f71d07c619dd7112bd42c8e86c86950ec47
michalcynarski <cynarski60@gmail.com>
2024-08-13 16:38:01 +0200

    Readme

--- ecomsite/ecomsite/settings.py
+++ ecomsite/ecomsite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +80,7 @@ WSGI_APPLICATION = 'ecomsite.wsgi.application'
 
 
 DATABASES = {
-    'sqlite': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
